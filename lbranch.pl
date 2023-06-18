d61727d9b8ddba3cb425c81a70a42feb4f6adc1d
Robert <robertkey@mighty.co.za>
2023-06-18 15:02:19 +0200

    placed code for extracting newest branch code in a sub

--- lbranch.pl
+++ lbranch.pl
@@ -7,16 +7,6 @@
 use strict;
 use warnings;
 
-# binfo contains (branch_name => commit_hash)
-# there is no order in a hash
-# hence @branch is used to keep an ordered list
-# of branches. Newest to oldest.
-my %binfo;
-
-# an ordered list of branches
-# newest first. a hash has no order
-my @branch = ();
-
 # sub to clone a git project
 # cloneg (projectname, targetdirectory)
 sub cloneg {
@@ -35,30 +25,50 @@ sub getremote {
 }
 
 
+# sub to determine latest commit of latest branch
+# and checkout the latest branch
+# sub to get latest branch with latest commit checked out
+# lbranch(ref_to_%binfo, ref to @branch, repository_name)
+sub lbranch { 
+	# get parameters
+	my ($rbinfo, $rbranch, $gname) = @_;
+	
+	# get heads and sort
+	# line 0 will be the latest head, line 1 next etc
+	my @line = `git ls-remote --heads --sort=-committerdate $gname`;
+
+	for (my $i =0; $i<= $#line; $i++) {
+
+		# each line contains commit, refs/heads/branch_name
+		# get branch 
+		my $branch = (split (/\//, $line[$i]))[2];
+		chomp($branch);
+
+		# make an ordered list of branches, newest first.
+		# a hash has no order
+		push(@{$rbranch}, $branch);
+		
+		# get the commit for the branch
+		$rbinfo->{$branch} = (split(/\s+/, $line[$i]))[0];
+	}
+}
+
+#####################################################################
+# main entry
+#####################################################################
+# binfo contains (branch_name => commit_hash)
+# there is no order in a hash
+# hence @branch is used to keep an ordered list
+# of branches. Newest to oldest.
+my %binfo;
+
+# an ordered list of branches
+# newest first. a hash has no order
+my @branch = ();
+
 # get remote name
 my $gname = getremote;
 
-# sub to determine latest commit of latest branch
-# and checkout the latest branch
-# 
-# get heads and sort
-# line 0 will be the latest head, line 1 next etc
-my @line = `git ls-remote --heads --sort=-committerdate $gname`;
-
-for (my $i =0; $i<= $#line; $i++) {
-
-	# each line contains commit, refs/heads/branch_name
-	# get branch 
-	my $branch = (split (/\//, $line[$i]))[2];
-	chomp($branch);
-
-	# make an ordered list of branches, newest first.
-	# a hash has no order
-	push(@branch, $branch);
-	
-	# get the commit for the branch
-	$binfo{$branch} = (split(/\s+/, $line[$i]))[0];
-}
 # print results from newest to oldest
 for (my $i = 0; $i <= $#branch; $i++) {
 	print "$branch[$i] => $binfo{$branch[$i]}\n";
