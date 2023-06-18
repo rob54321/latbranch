ed0d71a560ecee7267012ff1e5581b1a25b636f6
Robert <robertkey@mighty.co.za>
2023-06-18 19:58:48 +0200

    still developing

--- lbranch.pl
+++ lbranch.pl
@@ -1,17 +1,22 @@
 #!/usr/bin/perl
 # this script determines the branch with the latest branch
 # in a cloned project. The current branch may not be 
-# the latest branch. The lates branch is checked out
+# the latest branch. The latest branch is checked out
 # so the files will be from the last commit of the latest branch.
 
 use strict;
 use warnings;
 
 # sub to clone a git project
-# cloneg (projectname, targetdirectory)
+# cloneg (targetdirectory)
 sub cloneg {
 	# get parameters
-	my($pname, $branch, $directory) = @_;
+	my($directory) = $_[0];
+
+	# project name is project_name.git
+	# pname = project_name
+	# project name was passed as a command line parameter
+	my $pname = $ARGV[0];
 
 	system("git clone -n -v https://github.com/rob54321/$pname $directory") or die "Could not clone $pname: $!\n";
 }
@@ -31,17 +36,20 @@ sub getremote {
 # lbranch(ref_to_%binfo, ref to @branch, repository_name)
 sub lbranch { 
 	# get parameters
-	my ($rbinfo, $rbranch, $gname) = @_;
+	my ($rbinfo, $rbranch) = @_;
+
+	# get remote name
+	my $rname = getremote;
 	
 	# get heads and sort
 	# line 0 will be the latest head, line 1 next etc
-	my @line = `git ls-remote --heads --sort=-committerdate $gname`;
+	my @line = `git ls-remote --heads --sort=-committerdate $rname`;
 
 	for (my $i =0; $i<= $#line; $i++) {
 
 		# each line contains commit, refs/heads/branch_name
 		# get branch 
-		my $branch = (split (/\//, $line[$i]))[2];
+		my ${$rbranch} = (split (/\//, $line[$i]))[2];
 		chomp($branch);
 
 		# make an ordered list of branches, newest first.
@@ -51,11 +59,19 @@ sub lbranch {
 		# get the commit for the branch
 		$rbinfo->{$branch} = (split(/\s+/, $line[$i]))[0];
 	}
+	# print results from newest to oldest
+	for (my $i = 0; $i <= $#branch; $i++) {
+		print "$branch[$i] => $binfo{$branch[$i]}\n";
+	}
+
+	# print remote name and latest branch
+	print "remote: $gname\t latest branch: $branch[0]\n";
 }
 
 #####################################################################
 # main entry
 #####################################################################
+
 # binfo contains (branch_name => commit_hash)
 # there is no order in a hash
 # hence @branch is used to keep an ordered list
@@ -66,13 +82,14 @@ my %binfo;
 # newest first. a hash has no order
 my @branch = ();
 
-# get remote name
-my $gname = getremote;
+# target directory to clone into
+my $targetdir = "/home/robert/tmp/gproject";
+unlink $targetdir;
 
-# print results from newest to oldest
-for (my $i = 0; $i <= $#branch; $i++) {
-	print "$branch[$i] => $binfo{$branch[$i]}\n";
-}
+# clone the project
+clone $targetdirectory;
 
-# print remote name and latest branch
-print "remote: $gname\t latest branch: $branch[0]\n";
+# change directory to target directory after clone
+chdir $targetdirectory;
+
+
